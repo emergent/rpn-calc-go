e8af78b9658cfa4de4a1739901be7a45a6282476
Satoshi Yoshikawa <yoshikawa@fairydevices.jp>
2025-06-26 03:22:43 +0900

    Revert "Fix: macOS compatibility for build script and WebAssembly loading error"
    This reverts commit 096d68d8641d6fc1916916a0bbfa6063350e0c75.

--- build_and_run_wasm.sh
+++ build_and_run_wasm.sh
@@ -9,17 +9,16 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-# Copy wasm_exec.js if it doesn't exist
+# Copy wasm_exec.js if it doesn't exist or is outdated
 GO_ROOT=$(go env GOROOT)
-if [ ! -f wasm_exec.js ]; then
+if [ ! -f wasm_exec.js ] || [ "$(find "$GO_ROOT/misc/wasm/wasm_exec.js" -prune -printf '%Y')" -gt "$(find "wasm_exec.js" -prune -printf '%Y')" ]; then
     echo "Copying wasm_exec.js..."
     cp "$GO_ROOT/misc/wasm/wasm_exec.js" .
 else
-    echo "wasm_exec.js already exists."
+    echo "wasm_exec.js is up to date."
 fi
 
 # Start a simple HTTP server
 echo "Starting a simple HTTP server on port 8080..."
 echo "Open your browser to http://localhost:8080"
-python3 -m http.server 8080 || python -m SimpleHTTPServer 8080 # Try python3, then python2
-
+python3 -m http.server 8080
